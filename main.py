521a87ef14f92cf9509df691dbcdc8b1f1f5b2d5
susu <113452531+susanna3003@users.noreply.github.com>
2024-03-18 20:01:25 +0000

    changed a few things - no dictionary needed because DATABASE

--- main.py
+++ main.py
@@ -4,25 +4,33 @@ from datetime import date
 import uuid
 
 class Tasks:
-  def __init__(self, ID, taskName, taskType, createDate, dueDate, taskLocation, taskInvite, taskReminder):
+  def __init__(self, ID, taskName, taskType, createDate, taskDueDate, taskLocation, taskInvite, taskReminder):
     self.ID = uuid.uuid4()
     self.name = taskName
     self.type = taskType
     self.createDate = date.today()
-    self.dueDate = dueDate
+    self.dueDate = taskDueDate
     self.location = taskLocation
     self.invites = taskInvite
     self.reminder = taskReminder
 
+class Reminders:
+  def __init__(self, ID, reminderName, reminderCreateDate, reminderDueDate, reminderLocation, reminderInvite, reminderRecurr):
+    self.ID = uuid.uuid4()
+    self.name = reminderName
+    self.createDate = date.today()
+    self.dueDate = reminderDueDate
+    self.location = reminderLocation
+    self.invites = reminderInvite
+    self.recurr = reminderRecurr
+
 # Flask constructor
 app = Flask(__name__)   
 
-tasksDict = {}
-
 # A decorator used to tell the application
 # which URL is associated function
-@app.route('/', methods =["GET", "POST"])
-def newTask():
+@app.route('/taskHome', methods =["GET", "POST"])
+def createNewTask():
     if request.method == "POST":
        # getting task ID using UUID
        taskID = str(uuid.uuid4())
@@ -45,11 +53,35 @@ def newTask():
 
       # create task object
       newTask = Tasks(taskID, taskName, taskType, taskCreateDate, taskDateDue, taskLocation, taskInvite, taskRemind)
-
-      # store new task in task dict
-      tasksDict[taskID] = newTask
       
-    return render_template("taskForm.html")
+    return render_template("taskHome.html")
+
+# A decorator used to tell the application
+# which URL is associated function
+@app.route('/reminderHome', methods =["GET", "POST"])
+def createNewReminder():
+    if request.method == "POST":
+       # getting task ID using UUID
+       reminderID = str(uuid.uuid4())
+
+       # task create date using datetime
+       reminderCreateDate = date.today()
+
+       # getting input with taskName = taskName in HTML form
+       reminderName = request.form.get("reminderName")
+       # getting input with taskType = taskType in HTML form 
+       reminderDateDue = request.form.get("dateDue")
+       # getting input with taskType = taskType in HTML form 
+       reminderLocation = request.form.get("reminderLocation")
+       # getting input with taskType = taskType in HTML form 
+       reminderInvite = request.form.get("reminderInvite")
+       # getting input with taskType = taskType in HTML form 
+       reminderRecurr = request.form.get("reminderRecurr")
+
+      # create task object
+      newReminder = Reminders(reminderID, reminderName, reminderCreateDate, reminderDateDue, reminderLocation, reminderInvite, reminderRecurr)
+
+    return render_template("reminderHome.html")
 
 if __name__=='__main__':
    app.run()
